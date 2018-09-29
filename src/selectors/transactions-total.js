5138247cb66e26fc15b60f30229187e0fb8ac738
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-09-29 18:44:34 +0200

    fix transaction-total selector

--- src/selectors/transactions-total.js
+++ src/selectors/transactions-total.js
@@ -1,4 +1,15 @@
+import TransactionTypes from '../enums/TransactionType'
+
 export default transactions =>
   transactions
-    .map(transaction => transaction.amount)
+    .map(transaction => {
+      switch (transaction.type) {
+        case TransactionTypes.In:
+          return transaction.amount;
+        case TransactionTypes.Out:
+          return transaction.amount * -1;
+        default:
+          return 0;
+      }
+    })
     .reduce((sum, amount) => sum + amount, 0);
