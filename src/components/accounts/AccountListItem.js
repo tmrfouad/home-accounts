610f340595d68a6de27d5959ff197d8e894d26ed
tmrfouad <tmrfouad@gmail.com>
2018-11-05 14:17:33 +0200

    set the color for account balance in the accounts page

--- src/components/accounts/AccountListItem.js
+++ src/components/accounts/AccountListItem.js
@@ -8,7 +8,12 @@ const AccountListItem = ({ id, name, total, settings }) => (
     <div>
       <h3 className="list-item__title">{name}</h3>
     </div>
-    <h3 className="list-item__data">
+    <h3
+      className={
+        'list-item__data' +
+        (total === 0 ? '' : total < 0 ? ' negative' : ' positive')
+      }
+    >
       {settings.currencySymbol + numeral(total / 100).format('0,0.00')}
     </h3>
   </Link>
